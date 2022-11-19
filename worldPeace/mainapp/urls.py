c82d53940a96e5101ef0ed08e250593ea50f229a
icebergD <snfmxmsc.s@gmail.com>
2022-11-19 07:59:33 +0500

    make it better

--- worldPeace/mainapp/urls.py
+++ worldPeace/mainapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("", views.home.as_view(), name='home'),
     path("hello", views.hello),
@@ -14,4 +17,4 @@ urlpatterns = [
 
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', views.logout_user, name='logout'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
