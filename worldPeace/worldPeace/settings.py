709a9ca20bf1266c5bdc9636fd8a84d3aa54819b
icebergD <snfmxmsc.s@gmail.com>
2022-11-19 07:50:36 +0500

    make it better

--- worldPeace/worldPeace/settings.py
+++ worldPeace/worldPeace/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'worldPeace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd7tnc0mj3ctiip',
+        'USER': 'fqmjiukwgfjggv',
+        'PASSWORD': '158993a23d682898045243337ab3f8b5efc92ef9768f0083a72bb7e1873dec3b',
+        'HOST': 'ec2-23-20-224-166.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
